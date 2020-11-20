f1e3b1794d815b658c46a24329e89a954c5ff8a4
Lucia Mahutova <luciamahutova@gmail.com>
2020-11-20 13:14:55 +0100

    Created script for closing/opening the menu

---
+++ js/menuSidebarScript.js
@@ -0,0 +1,10 @@
+// Script for menu
+function openNav() {
+    document.getElementById("menu").style.width = "350px";
+}
+
+function closeNav() {
+    document.getElementById("menu").style.width = "0";
+}
+
+// Script for sidebar
