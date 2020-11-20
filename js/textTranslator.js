cc1028ac37de23c1740c94743b94076f5716ceba
Lucia Mahutova <luciamahutova@gmail.com>
2020-11-20 18:33:28 +0100

    Created textTranslator.js for translating web app (3 languages)

---
+++ js/textTranslator.js
@@ -0,0 +1,25 @@
+// var languages = {
+//     "CZ": "Čeština",
+//     "EN": "English",
+//     "SK": "Slovenčina"
+// };
+
+// var wordsInCZ = {
+//     "navText1": "Model sluneční soustavy",
+//     "navText2": "Planety",
+//     "navText3": "O aplikaci"
+// };
+
+// var wordsInEN = {
+//     "navText1": "Solar system model",
+//     "navText2": "Planets",
+//     "navText3": "About application"
+// };
+
+// var wordsInSK = {
+//     "navText1": "Model slnečnej sústavy",
+//     "navText2": "Planéty",
+//     "navText3": "O aplikácii"
+// };
+
+// window.onload = initialize;
