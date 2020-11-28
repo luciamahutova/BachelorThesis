7476a44be13bbd465e1b22d77ae3706d194fb446
Lucia Mahutova <luciamahutova@gmail.com>
2020-11-28 16:04:58 +0100

    Script planetsCopy.js - changed to OOP, working right

--- js/planetsCopy.js
+++ js/planetsCopy.js
@@ -1,140 +1,143 @@
-class Planet {
+function planet() {
+    // this.sun, this.moon, this.mercury, this.venus, this.earth, this.mars, this.jupiter, this.saturn, this.uranus,
+    //     this.neptune = null;
+    // this.sunMesh, this.moonMesh, this.mercuryMesh, this.venusMesh, this.earthMesh, this.marsMesh, this.jupiterMesh,
+    //     this.saturnMesh, this.uranusMesh, this.neptuneMesh = null;
+    // this.emptyObjectRotateMercury, this.emptyObjectRotateVenus, this.emptyObjectRotateEarth, this.emptyObjectRotateMoon,
+    //     this.emptyObjectRotateMars, this.emptyObjectRotateJupiter, this.emptyObjectRotateSaturn, this.emptyObjectRotateUranus, this.emptyObjectRotateNeptune = null;
+    // this.meshMaterial = null;
 
-}
+    this.createPlanets = function createPlanets() {
+        // 10x smaller scale for the Sun
+        this.sun = new THREE.SphereBufferGeometry(5, 50, 50);
+        this.mercury = new THREE.SphereBufferGeometry(0.175, 50, 50);
+        this.venus = new THREE.SphereBufferGeometry(0.435, 50, 50);
+        this.earth = new THREE.SphereBufferGeometry(0.457, 50, 50);
+        this.moon = new THREE.SphereBufferGeometry(0.124, 50, 50);
+        this.mars = new THREE.SphereBufferGeometry(0.243, 50, 50);
 
-
-var sun, moon, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune;
-var sunMesh, moonMesh, mercuryMesh, venusMesh, earthMesh, marsMesh, jupiterMesh, saturnMesh, uranusMesh, neptuneMesh;
-var emptyObjectRotateMercury, emptyObjectRotateVenus, emptyObjectRotateEarth, emptyObjectRotateMoon, emptyObjectRotateMars, emptyObjectRotateJupiter, emptyObjectRotateSaturn, emptyObjectRotateUranus, emptyObjectRotateNeptune;
-
-
-function createPlanets() {
-    // 10x smaller scale for the Sun
-    sun = new THREE.SphereBufferGeometry(5, 50, 50);
-    mercury = new THREE.SphereBufferGeometry(0.175, 50, 50);
-    venus = new THREE.SphereBufferGeometry(0.435, 50, 50);
-    earth = new THREE.SphereBufferGeometry(0.457, 50, 50);
-    moon = new THREE.SphereBufferGeometry(0.124, 50, 50);
-    mars = new THREE.SphereBufferGeometry(0.243, 50, 50);
-
-    // 3x smaller scale (4 planets)
-    jupiter = new THREE.SphereBufferGeometry(1.673, 50, 50);
-    saturn = new THREE.SphereBufferGeometry(1.394, 50, 50);
-    uranus = new THREE.SphereBufferGeometry(0.607, 50, 50);
-    neptune = new THREE.SphereBufferGeometry(0.589, 50, 50);
-    jupiter.receiveShadow = true;
-}
-
-// Mesh for planets
-function setNewMesh(imageSrc) {
-    let texture = new THREE.TextureLoader().load(imageSrc);
-    let meshMaterial = new THREE.MeshPhongMaterial({ map: texture });
-    meshMaterial.receiveShadow = true;
-    meshMaterial.castShadow = true;
-    return meshMaterial;
-}
-
-function createPlanetsMesh() {
-    sunMesh = new THREE.Mesh(sun, setNewMesh('/images/textures/sunTexture2k.jpg'));
-    mercuryMesh = new THREE.Mesh(mercury, setNewMesh('/images/textures/mercuryTexture2k.jpg'));
-    venusMesh = new THREE.Mesh(venus, setNewMesh('/images/textures/venusTexture2k.jpg'));
-    earthMesh = new THREE.Mesh(earth, setNewMesh('/images/textures/earthTexture2k.jpg'));
-    moonMesh = new THREE.Mesh(moon, setNewMesh('/images/textures/moonTexture2k.jpg'));
-    marsMesh = new THREE.Mesh(mars, setNewMesh('/images/textures/marsTexture2k.jpg'));
-    jupiterMesh = new THREE.Mesh(jupiter, setNewMesh('/images/textures/jupiterTexture2k.jpg'));
-    saturnMesh = new THREE.Mesh(saturn, setNewMesh('/images/textures/saturnTexture2k.jpg'));
-    uranusMesh = new THREE.Mesh(uranus, setNewMesh('/images/textures/uranusTexture2k.jpg'));
-    neptuneMesh = new THREE.Mesh(neptune, setNewMesh('/images/textures/neptuneTexture2k.jpg'));
-    // scene.add(sunMesh, moonMesh, mercuryMesh, venusMesh, earthMesh, marsMesh, jupiterMesh, saturnMesh, uranusMesh, neptuneMesh);
-    return [sunMesh, moonMesh, mercuryMesh, venusMesh, earthMesh, marsMesh, jupiterMesh, saturnMesh, uranusMesh, neptuneMesh];
-}
-
-function setPlanetsRotationAngle() {
-    // Planets' angle around its axis = Z
-    mercuryMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (0.03 * Math.PI) / 180);
-    venusMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (177.4 * Math.PI) / 180);
-    earthMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (23.4 * Math.PI) / 180);
-    marsMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (25.2 * Math.PI) / 180);
-    jupiterMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (3.1 * Math.PI) / 180);
-    saturnMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (26.7 * Math.PI) / 180);
-    uranusMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (97.8 * Math.PI) / 180);
-    neptuneMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (28.3 * Math.PI) / 180);
-}
-
-// Position for every planet's orbit from the Sun
-function setPlanetsDistanceFromSun(eulerNumberDistanceFromSun) {
-    // Changed scale for better view
-    mercuryMesh.position.x = eulerNumberDistanceFromSun[0] / 300;
-    venusMesh.position.x = eulerNumberDistanceFromSun[1] / 300;
-    earthMesh.position.x = eulerNumberDistanceFromSun[2] / 300;
-    marsMesh.position.x = eulerNumberDistanceFromSun[3] / 400;
-    jupiterMesh.position.x = eulerNumberDistanceFromSun[4] / 1000;
-    saturnMesh.position.x = eulerNumberDistanceFromSun[5] / 1500;
-    uranusMesh.position.x = eulerNumberDistanceFromSun[6] / 2500;
-    neptuneMesh.position.x = eulerNumberDistanceFromSun[7] / 3500;
-    setMoonDistanceFromEarth();
-}
-
-function setMoonDistanceFromEarth() {
-    // Not proper value = set according to model
-    moonMesh.position.x = 1;
-}
-
-//Empty objects will control planets' movement around the Sun
-function createEmptyObjects() {
-    emptyObjectRotateMercury = new THREE.Object3D();
-    emptyObjectRotateMercury.add(mercuryMesh);
-
-    emptyObjectRotateVenus = new THREE.Object3D();
-    emptyObjectRotateVenus.add(venusMesh);
-
-    emptyObjectRotateEarth = new THREE.Object3D();
-    emptyObjectRotateEarth.add(earthMesh);
-
-    emptyObjectRotateMoon = new THREE.Object3D();
-    earthMesh.add(emptyObjectRotateMoon);
-    emptyObjectRotateMoon.add(moonMesh);
-
-    emptyObjectRotateMars = new THREE.Object3D();
-    emptyObjectRotateMars.add(marsMesh);
-
-    emptyObjectRotateJupiter = new THREE.Object3D();
-    emptyObjectRotateJupiter.add(jupiterMesh);
-
-    emptyObjectRotateSaturn = new THREE.Object3D();
-    emptyObjectRotateSaturn.add(saturnMesh);
-
-    emptyObjectRotateUranus = new THREE.Object3D();
-    emptyObjectRotateUranus.add(uranusMesh);
-
-    emptyObjectRotateNeptune = new THREE.Object3D();
-    emptyObjectRotateNeptune.add(neptuneMesh);
-
-    addEmptyToSun();
-}
-
-// For rotating planets around the Sun
-function addEmptyToSun() {
-    var emptyObjectsArray = [emptyObjectRotateMercury, emptyObjectRotateVenus, emptyObjectRotateEarth, emptyObjectRotateMars, emptyObjectRotateJupiter, emptyObjectRotateSaturn, emptyObjectRotateUranus, emptyObjectRotateNeptune];
-
-    for (i = 0; i < emptyObjectsArray.length; i++) {
-        sunMesh.add(emptyObjectsArray[i]);
+        // 3x smaller scale (4 planets)
+        this.jupiter = new THREE.SphereBufferGeometry(1.673, 50, 50);
+        this.saturn = new THREE.SphereBufferGeometry(1.394, 50, 50);
+        this.uranus = new THREE.SphereBufferGeometry(0.607, 50, 50);
+        this.neptune = new THREE.SphereBufferGeometry(0.589, 50, 50);
     }
-}
 
-function setPlanetsRotationSpeedAroundSun(values) {
-    emptyObjectRotateMercury.rotation.y += values[0] / 100;
-    emptyObjectRotateVenus.rotation.y += values[1] / 100;
-    emptyObjectRotateEarth.rotation.y += values[2] / 100;
-    emptyObjectRotateMars.rotation.y += values[3] / 100;
-    emptyObjectRotateJupiter.rotation.y += values[4] / 100;
-    emptyObjectRotateSaturn.rotation.y += values[5] / 100;
-    emptyObjectRotateUranus.rotation.y += values[6] / 100;
-    emptyObjectRotateNeptune.rotation.y += values[7] / 100;
-    setMoonRotationAroundEarth();
-}
+    this.setNewMesh = function setNewMesh(imageSrc) {
+        let texture = new THREE.TextureLoader().load(imageSrc);
+        this.meshMaterial = new THREE.MeshPhongMaterial({ map: texture });
+        this.meshMaterial.receiveShadow = true;
+        this.meshMaterial.castShadow = true;
+        return this.meshMaterial;
+    }
 
-function setMoonRotationAroundEarth() {
-    // Not proper value, needs to change
-    emptyObjectRotateMoon.rotation.y += 0.001;
+    this.createPlanetsMesh = function createPlanetsMesh() {
+        this.sunMesh = new THREE.Mesh(this.sun, this.setNewMesh('/images/textures/sunTexture2k.jpg'));
+        this.mercuryMesh = new THREE.Mesh(this.mercury, this.setNewMesh('/images/textures/mercuryTexture2k.jpg'));
+        this.venusMesh = new THREE.Mesh(this.venus, this.setNewMesh('/images/textures/venusTexture2k.jpg'));
+        this.earthMesh = new THREE.Mesh(this.earth, this.setNewMesh('/images/textures/earthTexture2k.jpg'));
+        this.moonMesh = new THREE.Mesh(this.moon, this.setNewMesh('/images/textures/moonTexture2k.jpg'));
+        this.marsMesh = new THREE.Mesh(this.mars, this.setNewMesh('/images/textures/marsTexture2k.jpg'));
+        this.jupiterMesh = new THREE.Mesh(this.jupiter, this.setNewMesh('/images/textures/jupiterTexture2k.jpg'));
+        this.saturnMesh = new THREE.Mesh(this.saturn, this.setNewMesh('/images/textures/saturnTexture2k.jpg'));
+        this.uranusMesh = new THREE.Mesh(this.uranus, this.setNewMesh('/images/textures/uranusTexture2k.jpg'));
+        this.neptuneMesh = new THREE.Mesh(this.neptune, this.setNewMesh('/images/textures/neptuneTexture2k.jpg'));
+        // scene.add(sunMesh, moonMesh, mercuryMesh, venusMesh, earthMesh, marsMesh, jupiterMesh, saturnMesh, uranusMesh, neptuneMesh);
+        return [this.sunMesh, this.moonMesh, this.mercuryMesh, this.venusMesh, this.earthMesh, this.marsMesh,
+            this.jupiterMesh, this.saturnMesh, this.uranusMesh, this.neptuneMesh
+        ];
+    }
+
+    this.setPlanetsRotationAngle = function setPlanetsRotationAngle() {
+        // Planets' angle around its axis = Z
+        this.mercuryMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (0.03 * Math.PI) / 180);
+        this.venusMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (177.4 * Math.PI) / 180);
+        this.earthMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (23.4 * Math.PI) / 180);
+        this.marsMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (25.2 * Math.PI) / 180);
+        this.jupiterMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (3.1 * Math.PI) / 180);
+        this.saturnMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (26.7 * Math.PI) / 180);
+        this.uranusMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (97.8 * Math.PI) / 180);
+        this.neptuneMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), (28.3 * Math.PI) / 180);
+    }
+
+    this.setMoonDistanceFromEarth = function setMoonDistanceFromEarth() {
+        // Not proper value = set according to model
+        this.moonMesh.position.x = 1;
+    }
+
+    // Position for every planet's orbit from the Sun
+    this.setPlanetsDistanceFromSun = function setPlanetsDistanceFromSun(values) {
+        // Changed scale for better view
+        this.mercuryMesh.position.x = values[0] / 300;
+        this.venusMesh.position.x = values[1] / 300;
+        this.earthMesh.position.x = values[2] / 300;
+        this.marsMesh.position.x = values[3] / 400;
+        this.jupiterMesh.position.x = values[4] / 1000;
+        this.saturnMesh.position.x = values[5] / 1500;
+        this.uranusMesh.position.x = values[6] / 2500;
+        this.neptuneMesh.position.x = values[7] / 3500;
+        this.setMoonDistanceFromEarth();
+    }
+
+    //Empty objects will control planets' movement around the Sun
+    this.createEmptyObjects = function createEmptyObjects() {
+        this.emptyObjectRotateMercury = new THREE.Object3D();
+        this.emptyObjectRotateMercury.add(this.mercuryMesh);
+
+        this.emptyObjectRotateVenus = new THREE.Object3D();
+        this.emptyObjectRotateVenus.add(this.venusMesh);
+
+        this.emptyObjectRotateEarth = new THREE.Object3D();
+        this.emptyObjectRotateEarth.add(this.earthMesh);
+
+        this.emptyObjectRotateMoon = new THREE.Object3D();
+        this.earthMesh.add(this.emptyObjectRotateMoon);
+        this.emptyObjectRotateMoon.add(this.moonMesh);
+
+        this.emptyObjectRotateMars = new THREE.Object3D();
+        this.emptyObjectRotateMars.add(this.marsMesh);
+
+        this.emptyObjectRotateJupiter = new THREE.Object3D();
+        this.emptyObjectRotateJupiter.add(this.jupiterMesh);
+
+        this.emptyObjectRotateSaturn = new THREE.Object3D();
+        this.emptyObjectRotateSaturn.add(this.saturnMesh);
+
+        this.emptyObjectRotateUranus = new THREE.Object3D();
+        this.emptyObjectRotateUranus.add(this.uranusMesh);
+
+        this.emptyObjectRotateNeptune = new THREE.Object3D();
+        this.emptyObjectRotateNeptune.add(this.neptuneMesh);
+
+        this.addEmptyToSun();
+    }
+
+    // For rotating planets around the Sun
+    this.addEmptyToSun = function addEmptyToSun() {
+        var emptyObjectsArray = [this.emptyObjectRotateMercury, this.emptyObjectRotateVenus, this.emptyObjectRotateEarth, this.emptyObjectRotateMars, this.emptyObjectRotateJupiter, this.emptyObjectRotateSaturn,
+            this.emptyObjectRotateUranus, this.emptyObjectRotateNeptune
+        ];
+
+        for (i = 0; i < emptyObjectsArray.length; i++) {
+            this.sunMesh.add(emptyObjectsArray[i]);
+        }
+    }
+
+    this.setMoonRotationAroundEarth = function setMoonRotationAroundEarth() {
+        // Not proper value, needs to change
+        this.emptyObjectRotateMoon.rotation.y += 0.001;
+    }
+
+    this.setPlanetsRotationSpeedAroundSun = function setPlanetsRotationSpeedAroundSun(values) {
+        this.emptyObjectRotateMercury.rotation.y += values[0] / 100;
+        this.emptyObjectRotateVenus.rotation.y += values[1] / 100;
+        this.emptyObjectRotateEarth.rotation.y += values[2] / 100;
+        this.emptyObjectRotateMars.rotation.y += values[3] / 100;
+        this.emptyObjectRotateJupiter.rotation.y += values[4] / 100;
+        this.emptyObjectRotateSaturn.rotation.y += values[5] / 100;
+        this.emptyObjectRotateUranus.rotation.y += values[6] / 100;
+        this.emptyObjectRotateNeptune.rotation.y += values[7] / 100;
+        this.setMoonRotationAroundEarth();
+    }
+
 }
--- js/simulationScriptCopy.js
+++ js/simulationScriptCopy.js
@@ -8,12 +8,15 @@ initialize();
 resizeBackground();
 setLight();
 setStaticBackground();
-createPlanets();
-planetsMesh = createPlanetsMesh();
-addMeshToScene(planetsMesh);
-setPlanetsRotationAngle();
-createEmptyObjects();
+
 // createZoomEvent();
+var planetObject = new planet();
+planetObject.createPlanets();
+planetsMesh = planetObject.createPlanetsMesh();
+addMeshToScene(planetsMesh);
+planetObject.setPlanetsRotationAngle();
+planetObject.createEmptyObjects();
+
 
 
 function initialize() {
@@ -71,88 +74,17 @@ function setStaticBackground() {
     bgScene.add(bgMesh);
 }
 
-function createZoomEvent() {
-    // Zoom-in and zoom-out
-    document.addEventListener('wheel', (event) => {
-        camera.position.z += (event.deltaY / 2000) * 0.5;
-    });
-}
-
 function addMeshToScene(planetsMesh) {
     for (var i = 0; i < planetsMesh.length; i++) {
         scene.add(planetsMesh[i]);
     }
 }
 
-
-// POKUS = MIZNUCA CIARA
-var fadeMaterial = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    transparent: true,
-    opacity: 0.01
-});
-var fadePlane = new THREE.PlaneBufferGeometry(0.15, 0.15, 0);
-var fadeMesh = new THREE.Mesh(fadePlane, fadeMaterial);
-var emptyObjectFadePlane = new THREE.Object3D();
-emptyObjectFadePlane.add(fadeMesh);
-sunMesh.add(emptyObjectFadePlane);
-// var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1500);
-fadeMesh.position.z = -0.1; // Put plane in front of camera
-fadeMesh.renderOrder = -1; // Make plane render before particles
-
-// GULICKA PRE VYTVORENIE CIARY, ZATIAL NEKRESLI PRI POHYBE
-var orbitLine = new THREE.SphereBufferGeometry(0.15, 30, 30);
-var orbitColor = new THREE.MeshBasicMaterial({ color: 0xffffff });
-var planetOrbit = new THREE.Mesh(orbitLine, orbitColor);
-emptyObjectRotateJupiter.add(planetOrbit);
-
-
-/////////////////////////////////////////////////////////////////////
-var MAX_POINTS = 500;
-var positionsY = new Array(500);
-var orbitLine = new THREE.SphereBufferGeometry(0.5, 30, 30);
-var orbitColor = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-var dot = new THREE.Mesh(orbitLine, orbitColor);
-var obj = new THREE.Object3D();
-
-function updatePositionsJupiter() {
-    var currentPosY = formNum = 0;
-
-    for (var i = 0, l = MAX_POINTS; i < l; i++) {
-        var currentPosY = emptyObjectRotateJupiter.rotation.y;
-        var formNum = (Math.round(currentPosY * 100) / 100).toFixed(4);
-
-        // if (positionsY.includes(formNum) === false) {
-        //     //planetOrbit.rotation.y += rotationValuesAroundSun[4] / 100;
-        //     console.log("formed: " + formNum);
-        //     console.log("posY: " + positionsY[i]);
-        // }
-        positionsY[i] = formNum;
-    }
-}
-
-function drawDot() {
-    obj.add(dot);
-    sunMesh.add(obj);
-    for (i = 0; i < positionsY.length; i++) {
-        obj.rotation.y = positionsY[i];
-        dot.position.x = eulerNumberDistanceFromSun[4] / 1000;
-        dot.position.z = 3;
-    }
-}
-
 // Animation
 ///////////////////////////////////////////////////////////////
 function animate() {
-    setPlanetsDistanceFromSun(eulerNumberDistanceFromSun);
-    setPlanetsRotationSpeedAroundSun(rotationValuesAroundSun);
-
-    // POKUS
-    planetOrbit.position.x = eulerNumberDistanceFromSun[4] / 1000;
-    planetOrbit.position.z = 2;
-    planetOrbit.rotation.y += rotationValuesAroundSun[4] / 100;
-    updatePositionsJupiter();
-    drawDot();
+    planetObject.setPlanetsDistanceFromSun(eulerNumberDistanceFromSun);
+    planetObject.setPlanetsRotationSpeedAroundSun(rotationValuesAroundSun);
 
     bgMesh.material.depthTest = false;
     renderer.autoClear = false;
