ff28466170ffdde269c3551cebf79aa469605e07
Lucia Mahutova <luciamahutova@gmail.com>
2020-12-05 20:20:25 +0100

    Translation for text showed in planetInfo.html

--- js/textTranslator.js
+++ js/textTranslator.js
@@ -23,9 +23,36 @@ let arrLang = {
         'earth': 'Země',
         'uranus': 'Uran',
         'neptune': 'Neptun',
-        'confirm': 'Potvrdit'
+        'confirm': 'Potvrdit',
+        // Planets
+        'diameter': 'Rovníkový poloměr:',
+        'mass': 'Hmotnost:',
+        'earths': '',
+        'moons': 'Měsíce:',
+        'none': 'Žádný',
+        'orbitDistance': 'Vzdálenost dráhy:',
+        'orbitPeriod': 'Oběžná doba:',
+        'days': '',
+        'years': '',
+        'surfaceTemperature': 'Povrchová teplota:',
+        'firstRecord': 'První záznam:',
+        'century': '',
+        'rings': 'Prstence:',
+        'exactDate': '',
+        'discovery': ''
     },
+    // Translation for long text
+    cz: {
+        'mercuryText': 'Merkur je planeta nejbližší ke Slunci. Na každé dvě oběžné dráhy Slunce absolvuje Merkur tři rotace kolem své osy. Třináctkrát za století lze pozorovat Merkur ze Země procházející kolem Slunce. Tato událost se nazývá tranzit.',
+        'venusText': 'Venuše je druhou planetou od Slunce a je druhou největší suchozemskou planetou. Venuše se pro svou podobnou velikost a hmotnost někdy označuje jako sesterská planeta Země. Venuše je pojmenovaná po římské bohyni lásky a krásy.',
+        'earthText': 'Země je třetí planetou od Slunce a je největší ze suchozemských planet. Země je jedinou planetou v naší sluneční soustavě, která není pojmenována podle řeckého nebo římského božství. Zemi vznikla asi před 4,54 miliardami let a je jedinou známou planetou, která podporuje život.',
+        'marsText': 'Mars je čtvrtá planeta od Slunce a je druhou nejmenší planetou ve sluneční soustavě. Mars, pojmenovaný podle římského boha války, je kvůli načervenalý vzhledu také často označován jako "Červená planeta". Mars je suchozemská planeta s řídkou atmosférou složenou především z oxidu uhličitého.',
+        'jupiterText': 'Jupiter je největší planetou ve sluneční soustavě a je pátou planetou od Slunce. Je dvaapůlkrát hmotnější než všechny ostatní planety ve sluneční soustavě dohromady. Primární je tvořen z plynů, a proto je známý jako "plynový gigant".',
+        'saturnText': 'Saturn, který je nejlépe známý díky svému pohádkovému prstencová systému, je šestou planetou od Slunce a druhou největší v naší sluneční soustavě. Stejně jako Jupiter, i Saturn je plynný gigant a je složen z podobných plynů včetně vodíku, hélia a metanu.',
+        'uranusText': 'Uran je sedmá planeta od Slunce. Není viditelná pouhým okem a stala se první planetou objevenou pomocí dalekohledu. Uran je převrácený na své straně s axiálním sklonem 98 stupňů. Často se to popisuje jako "valení se kolem Slunce na jeho boku".',
+        'neptune': 'Neptun je osmou planetou od Slunce, díky čemuž je nejvzdálenější ve sluneční soustavě. Tento plynný gigant mohl v ranní historii sluneční soustavy zformovat mnohem blíže ke Slunci, než se dostal do své současné polohy.'
 
+    },
     en: {
         // Menu
         'ssm': 'Solar system model',
@@ -50,7 +77,34 @@ let arrLang = {
         'earth': 'Earth',
         'uranus': 'Uranus',
         'neptune': 'Neptune',
-        'confirm': 'Confirm'
+        'confirm': 'Confirm',
+        // Planets
+        'diameter': 'Equatorial diameter:',
+        'mass': 'Mass:',
+        'earths': '',
+        'moons': 'Moons:',
+        'none': 'None',
+        'orbitDistance': 'Orbit distance:',
+        'orbitPeriod': 'Orbit period:',
+        'days': '',
+        'years': '',
+        'surfaceTemperature': 'Surface temperature:',
+        'firstRecord': 'First record',
+        'century': '',
+        'rings': 'Rings',
+        'exactDate': '',
+        'discovery': ''
+    },
+    // Translation for long text
+    en: {
+        'mercuryText': 'Mercury is the closest planet to the Sun. For every two orbits of the Sun, Mercury completes three      rotations about its axis. Thirteen times a century Mercury can be observed from the Earth passing across the face of the Sun in an event called a transit.',
+        'venusText': 'Venus is the second planet from the Sun and is the second largest terrestrial planet. Venus is sometimes referred to as the Earth’s sister planet due to their similar size and mass. Venus is named after the Roman goddess of love and beauty.',
+        'earthText': 'Earth is the third planet from the Sun and is the largest of the terrestrial planets. The Earth is the only planet in our solar system not to be named after a Greek or Roman deity. The Earth was formed approximately 4.54 billion years ago and is the only known planet to support life.',
+        'marsText': 'Mars is the fourth planet from the Sun and is the second smallest planet in the solar system. Named after the Roman god of war, Mars is also often described as the “Red Planet” due to its reddish appearance. Mars is a terrestrial planet with a thin atmosphere composed primarily of carbon dioxide.',
+        'jupiterText': 'Jupiter is the largest planet in the solar system and is the fifth planet out from the Sun. It is two and a half times more massive than all the other planets in the solar system combined. It is made primarily of gases and is therefore known as a “gas giant”.',
+        'saturnText': 'Best known for its fabulous ring system, Saturn is the sixth planet from the Sun and the second largest in our solar system. Like Jupiter, Saturn is a gas giant and is composed of similar gasses including hydrogen, helium and methane.',
+        'uranusText': 'Uranus is the seventh planet from the Sun. It’s not visible to the naked eye, and became the first planet discovered with the use of a telescope. Uranus is tipped over on its side with an axial tilt of 98 degrees. It is often described as “rolling around the Sun on its side.”',
+        'neptune': 'Neptune is the eighth planet from the Sun, making it the most distant in the solar system. This gas giant may have formed much closer to the Sun in the early solar system history before migrating out to its current position.'
     },
     sk: {
         // Menu
@@ -76,7 +130,34 @@ let arrLang = {
         'earth': 'Zem',
         'uranus': 'Urán',
         'neptune': 'Neptún',
-        'confirm': 'Potvrdiť'
+        'confirm': 'Potvrdiť',
+        // Planets
+        'diameter': 'Rovníkový polomer:',
+        'mass': 'Hmotnosť:',
+        'earths': '',
+        'moons': 'Mesiace:',
+        'none': 'Žiadny',
+        'orbitDistance': 'Vzdialenosť dráhy:',
+        'orbitPeriod': 'Obežná doba:',
+        'days': '',
+        'years': '',
+        'surfaceTemperature': 'Povrchová teplota:',
+        'firstRecord': 'Prvý záznam:',
+        'century': '',
+        'rings': 'Prstence:',
+        'exactDate': '',
+        'discovery': ''
+    },
+    // Translation for long text
+    sk: {
+        'mercuryText': 'Merkúr je planéta najbližšia k Slnku. Na každé dve obežné dráhy Slnka absolvuje Merkúr tri rotácie okolo svojej osi. Trinásťkrát za storočie je možné pozorovať Merkúr zo Zeme prechádzajúcej okolo Slnka. Táto udalosť sa nazýva tranzit.',
+        'venusText': 'Venuša je druhou planétou od Slnka a je druhou najväčšou suchozemskou planétou. Venuša sa pre svoju podobnú veľkosť a hmotnosť niekedy označuje ako sesterská planéta Zeme. Venuša je pomenovaná po rímskej bohyni lásky a krásy.',
+        'earthText': 'Zem je treťou planétou od Slnka a je najväčšou zo suchozemských planét. Zem je jedinou planétou v našej slnečnej sústave, ktorá nie je pomenovaná podľa gréckeho alebo rímskeho božstva. Zem vznikla asi pred 4,54 miliardami rokov a je jedinou známou planétou, ktorá podporuje život.',
+        'marsText': 'Mars je štvrtá planéta od Slnka a je druhou najmenšou planétou v slnečnej sústave. Mars, pomenovaný podľa rímskeho boha vojny, je kvôli červenkastému vzhľadu tiež často označovaný ako „Červená planéta“. Mars je suchozemská planéta s riedkou atmosférou zloženou predovšetkým z oxidu uhličitého.',
+        'jupiterText': 'Jupiter je najväčšou planétou v slnečnej sústave a je piatou planétou od Slnka. Je dva a polkrát hmotnejšia ako všetky ostatné planéty v slnečnej sústave dokopy. Primárne je tvorený z plynov, a preto je známy ako „plynový gigant“.',
+        'saturnText': 'Saturn, ktorý je najlepšie známy vďaka svojmu rozprávkovému prstencovému systému, je šiestou planétou od Slnka a druhou najväčšou v našej slnečnej sústave. Rovnako ako Jupiter, aj Saturn je plynný gigant a je zložený z podobných plynov vrátane vodíka, hélia a metánu.',
+        'uranusText': 'Urán je siedma planéta od Slnka. Nie je viditeľná voľným okom a stala sa prvou planétou objavenou pomocou ďalekohľadu. Urán je prevrátený na svojej strane s axiálnym sklonom 98 stupňov. Často sa to popisuje ako „valenie sa okolo Slnka na jeho boku“.',
+        'neptune': 'Neptún je ôsmou planétou od Slnka, vďaka čomu je najvzdialenejšou v slnečnej sústave. Tento plynný gigant sa mohol v rannej histórii slnečnej sústavy sformovať oveľa bližšie k Slnku, než sa dostal do svojej súčasnej polohy.'
     }
 }
 
