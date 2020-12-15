605a478156c7e9ee3bb56b7f0c458304bc86aefb
Lucia Mahutova <luciamahutova@gmail.com>
2020-12-15 11:50:43 +0100

    Deleted array of values in main.js (not needed) - values are stored in dataValues (planets.js)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Values /////////////////////////////////////////////////////
 const rotationValuesAroundSun = [1.607, 1.174, 1.000, 0.802, 0.434, 0.323, 0.228, 0.182];
-const eulerNumberDistanceFromSun = [2.0790e+3, 3.8849e+3, 5.3709e+3, 8.1834e+3, 2.7951e+4, 5.1464e+4, 1.0328e+5, 1.6168e+5];
 var scene, renderer, camera, planetsMesh, zoomValue;
 
 var mainScene = new MainScene();
@@ -14,7 +13,7 @@ var planetObject = new Planet(scene);
 planetObject.initializePlanets();
 
 function animate() {
-    planetObject.setPlanetsRotationSpeedAroundSun(rotationValuesAroundSun);
+    //planetObject.setPlanetsRotationSpeedAroundSun(rotationValuesAroundSun);
 
     mainScene.animate();
     animationFrameOutput = requestAnimationFrame(animate);
