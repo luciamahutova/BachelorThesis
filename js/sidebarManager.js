9be5a8ecaefa2d227788b6133dc382d9bebb2950
Lucia Mahutova <luciamahutova@gmail.com>
2021-02-25 16:26:19 +0100

    New functions for moons on scene (Sidebar)

--- js/sidebarManager.js
+++ js/sidebarManager.js
@@ -114,6 +114,7 @@ SidebarManager.prototype.confirmButtonBehavior = function() {
     this.showHideSingleNameOfMoonOnScene();
 
     this.showHideAllMoonsOnScene();
+    this.showHideMoonsOfSinglePlanet();
 
     this.showHideAllPlanetOrbitsOnScene();
     this.showHideSinglePlanetOrbitOnScene();
@@ -128,10 +129,7 @@ SidebarManager.prototype.showHideAllNamesOfPlanetsOnScene = function() {
             this.planetsNamesOnScene[4], this.planetsNamesOnScene[5],
             this.planetsNamesOnScene[6], this.planetsNamesOnScene[7]);
     } else if (document.getElementById("allPlanetNamesChecked").checked == false) {
-        this.scene.remove(this.planetsNamesOnScene[0], this.planetsNamesOnScene[1],
-            this.planetsNamesOnScene[2], this.planetsNamesOnScene[3],
-            this.planetsNamesOnScene[4], this.planetsNamesOnScene[5],
-            this.planetsNamesOnScene[6], this.planetsNamesOnScene[7]);
+        this.removeAllNamesScene(this.planetsNamesOnScene);
     }
 }
 
@@ -140,25 +138,16 @@ SidebarManager.prototype.showHideSingleNameOfPlanetOnScene = function() {
     var allPlanetNamesSelected = document.getElementById("allPlanetNamesChecked").checked;
     // -1 is for option with no planet name
     if (selectedElem.value != -1 && allPlanetNamesSelected == false) {
-        this.scene.remove(this.planetsNamesOnScene[0], this.planetsNamesOnScene[1],
-            this.planetsNamesOnScene[2], this.planetsNamesOnScene[3],
-            this.planetsNamesOnScene[4], this.planetsNamesOnScene[5],
-            this.planetsNamesOnScene[6], this.planetsNamesOnScene[7]);
+        this.removeAllNamesScene(this.planetsNamesOnScene);
         this.scene.add(this.planetsNamesOnScene[selectedElem.value]);
     }
 }
 
 SidebarManager.prototype.showHideAllNamesOfMoonsOnScene = function() {
     if (document.getElementById("allMoonNamesChecked").checked == true) {
-        this.scene.add(this.moonsNamesOnScene[0], this.moonsNamesOnScene[1], this.moonsNamesOnScene[2],
-            this.moonsNamesOnScene[3], this.moonsNamesOnScene[4], this.moonsNamesOnScene[5], this.moonsNamesOnScene[6],
-            this.moonsNamesOnScene[7], this.moonsNamesOnScene[8], this.moonsNamesOnScene[9], this.moonsNamesOnScene[10],
-            this.moonsNamesOnScene[11], this.moonsNamesOnScene[12], this.moonsNamesOnScene[13], this.moonsNamesOnScene[14]);
+        this.addAllToScene(this.moonsNamesOnScene);
     } else if (document.getElementById("allMoonNamesChecked").checked == false) {
-        this.scene.remove(this.moonsNamesOnScene[0], this.moonsNamesOnScene[1], this.moonsNamesOnScene[2],
-            this.moonsNamesOnScene[3], this.moonsNamesOnScene[4], this.moonsNamesOnScene[5], this.moonsNamesOnScene[6],
-            this.moonsNamesOnScene[7], this.moonsNamesOnScene[8], this.moonsNamesOnScene[9], this.moonsNamesOnScene[10],
-            this.moonsNamesOnScene[11], this.moonsNamesOnScene[12], this.moonsNamesOnScene[13], this.moonsNamesOnScene[14]);
+        this.removeAllToScene(this.moonsNamesOnScene);
     }
 }
 
@@ -167,25 +156,61 @@ SidebarManager.prototype.showHideSingleNameOfMoonOnScene = function() {
     var allMoonNamesSelected = document.getElementById("allMoonNamesChecked").checked;
 
     if (selectedElem.value != -1 && allMoonNamesSelected == false) {
-        this.scene.remove(this.moonsNamesOnScene[0], this.moonsNamesOnScene[1], this.moonsNamesOnScene[2],
-            this.moonsNamesOnScene[3], this.moonsNamesOnScene[4], this.moonsNamesOnScene[5], this.moonsNamesOnScene[6],
-            this.moonsNamesOnScene[7], this.moonsNamesOnScene[8], this.moonsNamesOnScene[9], this.moonsNamesOnScene[10],
-            this.moonsNamesOnScene[11], this.moonsNamesOnScene[12], this.moonsNamesOnScene[13], this.moonsNamesOnScene[14]);
+        this.removeAllToScene(this.moonsNamesOnScene);
         this.scene.add(this.moonsNamesOnScene[selectedElem.value]);
     }
 }
 
+// Function for removing all planet names
+SidebarManager.prototype.removeAllNamesScene = function(objects) {
+    this.scene.remove(objects[0], objects[1], objects[2], objects[3], objects[4], objects[5], objects[6], objects[7]);
+}
+
+// Functions for moons or moon names (textures)
+SidebarManager.prototype.addAllToScene = function(objects) {
+    this.scene.add(objects[0], objects[1], objects[2], objects[3], objects[4], objects[5], objects[6], objects[7],
+        objects[8], objects[9], objects[10], objects[11], objects[12], objects[13], objects[14]);
+}
+
+SidebarManager.prototype.removeAllToScene = function(objects) {
+    this.scene.remove(objects[0], objects[1], objects[2], objects[3], objects[4], objects[5], objects[6], objects[7],
+        objects[8], objects[9], objects[10], objects[11], objects[12], objects[13], objects[14]);
+}
+
+// Sidebar - functions for moons
+// -------------------------------------------------------------------------
 SidebarManager.prototype.showHideAllMoonsOnScene = function() {
     if (document.getElementById("allMoonObjectsChecked").checked == true) {
-        this.scene.add(this.moonMeshes[0], this.moonMeshes[1], this.moonMeshes[2], this.moonMeshes[3],
-            this.moonMeshes[4], this.moonMeshes[5], this.moonMeshes[6], this.moonMeshes[7],
-            this.moonMeshes[8], this.moonMeshes[9], this.moonMeshes[10], this.moonMeshes[11],
-            this.moonMeshes[12], this.moonMeshes[13], this.moonMeshes[14]);
+        this.addAllToScene(this.moonMeshes);
     } else if (document.getElementById("allMoonObjectsChecked").checked == false) {
-        this.scene.remove(this.moonMeshes[0], this.moonMeshes[1], this.moonMeshes[2], this.moonMeshes[3],
-            this.moonMeshes[4], this.moonMeshes[5], this.moonMeshes[6], this.moonMeshes[7],
-            this.moonMeshes[8], this.moonMeshes[9], this.moonMeshes[10], this.moonMeshes[11],
-            this.moonMeshes[12], this.moonMeshes[13], this.moonMeshes[14]);
+        this.removeAllToScene(this.moonMeshes);
+        this.removeAllToScene(this.moonsNamesOnScene);
+    }
+}
+
+SidebarManager.prototype.showHideMoonsOfSinglePlanet = function() {
+    var selectedElem = document.getElementById("singlePlanetSelectedForMoons");
+    var allMoonObjectsChecked = document.getElementById("allMoonObjectsChecked").checked;
+
+    if (selectedElem.value != -1 && allMoonObjectsChecked == false) {
+        this.removeAllToScene(this.moonMeshes);
+        this.removeAllToScene(this.moonsNamesOnScene);
+        if (selectedElem.value == 2) {
+            this.scene.add(this.moonMeshes[0], this.moonsNamesOnScene[0]);
+        } else if (selectedElem.value == 4) {
+            this.scene.add(this.moonMeshes[1], this.moonMeshes[2], this.moonMeshes[3], this.moonMeshes[4]);
+            this.scene.add(this.moonsNamesOnScene[1], this.moonsNamesOnScene[2], this.moonsNamesOnScene[3], this.moonsNamesOnScene[4]);
+        } else if (selectedElem.value == 5) {
+            this.scene.add(this.moonMeshes[5], this.moonMeshes[6], this.moonMeshes[7], this.moonMeshes[8], this.moonMeshes[9]);
+            this.scene.add(this.moonsNamesOnScene[5], this.moonsNamesOnScene[6], this.moonsNamesOnScene[7],
+                this.moonsNamesOnScene[8], this.moonsNamesOnScene[9]);
+        } else if (selectedElem.value == 6) {
+            this.scene.add(this.moonMeshes[10], this.moonMeshes[11], this.moonMeshes[12], this.moonMeshes[12]);
+            this.scene.add(this.moonsNamesOnScene[10], this.moonsNamesOnScene[11],
+                this.moonsNamesOnScene[12], this.moonsNamesOnScene[12]);
+        } else if (selectedElem.value == 7) {
+            this.scene.add(this.moonMeshes[14], this.moonsNamesOnScene[14]);
+        }
     }
 }
 
