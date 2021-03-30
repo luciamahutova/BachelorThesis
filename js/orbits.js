00bdd8e9a80033cb180881414834340f880e4f3d
Lucia Mahutova <luciamahutova@gmail.com>
2021-03-30 20:13:35 +0200

    Update: function createCurveForOrbit() is divided into 2 parts and uses async() - for using data from Promise

--- js/orbits.js
+++ js/orbits.js
@@ -27,25 +27,35 @@ class Orbits extends JSONManager {
     }
 
     createCurveForOrbit(objectName, orbits, scene, dataJSON) {
-        var curve, geometry, material, ellipse;
+        var curve, promiseValue;
         var dataOfCurrentOrbitJSON = dataJSON;
 
-        dataOfCurrentOrbitJSON.then(function(result) {
-            curve = new THREE.EllipseCurve( // values for ellipse curve
-                result[objectName]["c"], 0, // aX, aY (X/Y center of the ellipse)
-                result[objectName]["a"] * result[objectName]["scaleFactor"], //xRadius 
-                result[objectName]["b"] * result[objectName]["scaleFactor"], //yRadius 
-                0, 2 * Math.PI, // aStartAngle, aEndAngle (angle of the curve in radians starting from the positive X axis)
-                false, 0 // aClockwise, aRotation
-            );
-            geometry = new THREE.BufferGeometry().setFromPoints(curve.getPoints(500));
-            material = new THREE.LineBasicMaterial({ color: 0xffffff });
-            ellipse = new THREE.Line(geometry, material);
-            ellipse.rotation.x = THREE.Math.degToRad(90);
-            ellipse.name = objectName;
-            orbits.push(ellipse);
+        (async() => {
+            promiseValue = dataOfCurrentOrbitJSON.then(function(result) {
+                curve = new THREE.EllipseCurve( // values for ellipse curve
+                    result[objectName]["c"], 0, // aX, aY (X/Y center of the ellipse)
+                    result[objectName]["a"] * result[objectName]["scaleFactor"], //xRadius 
+                    result[objectName]["b"] * result[objectName]["scaleFactor"], //yRadius 
+                    0, 2 * Math.PI, // aStartAngle, aEndAngle (angle of the curve in radians starting from the positive X axis)
+                    false, 0 // aClockwise, aRotation
+                );
+            });
+            await promiseValue;
+
+            var ellipse = this.setParamsForOrbitCurve(objectName, orbits, curve);
             scene.add(ellipse);
-        });
+        })();
+    }
+
+    setParamsForOrbitCurve(objectName, orbits, curve) {
+        var geometry, material, ellipse;
+        geometry = new THREE.BufferGeometry().setFromPoints(curve.getPoints(500));
+        material = new THREE.LineBasicMaterial({ color: 0xffffff });
+        ellipse = new THREE.Line(geometry, material);
+        ellipse.rotation.x = THREE.Math.degToRad(90);
+        ellipse.name = objectName;
+        orbits.push(ellipse);
+        return ellipse;
     }
 
     // Position moon orbits
